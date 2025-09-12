4ab0f4fd255133f4da17489dae3a81721d59d347
baoziwu2 <baoziwu2@foxmail.com>
2025-09-12 10:42:39 +0800

    project1

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,4 +1,31 @@
 package deque;
+import java.util.Comparator;
 
-public class MaxArrayDeque {
+public class MaxArrayDeque<typeName> extends ArrayDeque<typeName> {
+
+    private Comparator<typeName> comparator;
+
+    public MaxArrayDeque(Comparator<typeName> c) {
+        super();
+        this.comparator = c;
+    }
+
+    public typeName max(Comparator<typeName> cmp) {
+        if(isEmpty()) {
+            return null;
+        }
+
+        typeName maxSoFar = get(0);
+        for (int i = 1; i < size(); i++) {
+            typeName currentItem = get(i);
+            if (cmp.compare(currentItem, maxSoFar) > 0) {
+                maxSoFar = currentItem;
+            }
+        }
+        return maxSoFar;
+    }
+
+    public typeName max() {
+        return max(comparator);
+    }
 }
