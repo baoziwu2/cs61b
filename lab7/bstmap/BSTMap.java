ae4126abef5d81e2e1e4b4840e99bc120c84e8a8
baoziwu2 <baoziwu2@foxmail.com>
2025-09-18 13:26:17 +0800

    lab7

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,11 +1,17 @@
 package bstmap;
 
+import edu.princeton.cs.algs4.BST;
+import org.w3c.dom.Node;
+
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     private BSTNode root;
+    private HashSet<K> keySet = new HashSet<>();
+
     private class BSTNode {
         K key;
         V value;
@@ -62,6 +68,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     public void put(K key, V value) {
         if(key == null) throw new IllegalArgumentException("key cannot be null");
         root = put(key, value, root);
+        keySet.add(key);
     }
 
     private BSTNode put(K key, V value, BSTNode node) {
@@ -70,21 +77,62 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         if(cmp < 0) node.leftSon = put(key, value, node.leftSon);
         else if(cmp > 0) node.rightSon = put(key, value, node.rightSon);
         else node.value = value;
-        node.subTreeSize = size(node.leftSon) + size(node.rightSon) + 1;
+        sizeUpate(node);
         return node;
+    }
+
+    void sizeUpate(BSTNode node) {
+        node.subTreeSize = size(node.leftSon) + size(node.rightSon) + 1;
     }
 
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException("Not supported yet.");
+       return keySet;
     }
 
-    public V remove(K key) { // TODO
-        throw new UnsupportedOperationException();
+    public V remove(K key) {
+        if(key == null) throw new IllegalArgumentException("key cannot be null");
+        V value = get(key);
+        keySet.remove(key);
+        root = remove(key, root);
+        keySet.remove(key);
+        return value;
     }
 
-    public V remove(K key, V value) { // TODO
-        throw new UnsupportedOperationException();
+    public V remove(K key, V value){
+        if (key == null) throw new IllegalArgumentException("");
+        if (value != get(key)) return value;
+        return remove(key);
+    }
+
+    private BSTNode remove(K key, BSTNode node) { // TODO
+        if(key == null) throw new IllegalArgumentException("key cannot be null");
+        int cmp = key.compareTo(node.key);
+        if(cmp  < 0) node.leftSon = remove(key, node.leftSon);
+        else if(cmp > 0) node.rightSon = remove(key, node.rightSon);
+        else {
+            if(node.leftSon == null) return node.rightSon;
+            if(node.rightSon == null) return node.leftSon;
+
+            BSTNode tempNode = node;
+            node = searchMinNode(tempNode.rightSon);
+            node.rightSon = removeMin(tempNode.rightSon);
+            node.leftSon = tempNode.leftSon;
+        }
+        sizeUpate(node);
+        return node;
+    }
+
+    BSTNode searchMinNode(BSTNode node) {
+        if(node.leftSon == null) return node;
+        return searchMinNode(node.leftSon);
+    }
+
+    BSTNode removeMin(BSTNode node) {
+        if(node.leftSon == null) return node.rightSon;
+        node.leftSon = removeMin(node.leftSon);
+        sizeUpate(node);
+        return node;
     }
 
     @Override
@@ -105,11 +153,20 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
         public K next() {
             if(!hasNext()) throw new NoSuchElementException();
-            throw new UnsupportedOperationException("Not supported yet."); // TODO
+            K minKey = searchMinNode(node).key;
+            node = BSTMap.this.remove(minKey, node);
+            return minKey;
         }
     }
 
     public void printInOrder() {
-        // TODO
+        printInOrderWithNode(root);
+    }
+
+    void printInOrderWithNode(BSTNode node) {
+        if(node == null) return ;
+        printInOrderWithNode(node.leftSon);
+        System.out.println("key: " + node.key + " value: " + node.value);
+        printInOrderWithNode(node.rightSon);
     }
 }
